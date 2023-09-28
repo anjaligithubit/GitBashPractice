5c98228f2109cdc987dfefee5dff8f32716ce304
anjaligithubit <anjalijena66@gmail.com>
2023-09-28 13:51:56 +0530

    files added

---
+++ src/test/java/com/practicePackage/AnjaliTest.java
@@ -0,0 +1,11 @@
+package com.practicePackage;
+
+import org.testng.annotations.Test;
+
+public class AnjaliTest {
+	@Test 
+	public void anjali() {
+		System.out.println("--- anjali-----");
+	}
+
+}
---
+++ src/test/java/com/practicePackage/SubhadipTest.java
@@ -0,0 +1,11 @@
+package com.practicePackage;
+
+import org.testng.annotations.Test;
+
+public class SubhadipTest {
+	@Test
+	public void subhadip() {
+		System.out.println("--- subhadip---");
+	}
+
+}
